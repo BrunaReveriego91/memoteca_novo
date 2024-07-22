2f77dfaa9d305a75b00c003eca5a9cbe6f5adf39
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2024-07-22 16:59:40 -0300

    adicionado http params paginacao de ideias

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -12,8 +12,18 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API)
+  listar(pagina: number): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set("_page",pagina)
+      .set("_limit",itensPorPagina);
+
+    // return this.http
+    //   .get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)
+
+    return this.http
+        .get<Pensamento[]>(this.API, {params: params});
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
